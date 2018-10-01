b2d925220851138c222c6c10bc1b9900f5150249
김지원 <42952244+ds020228@users.noreply.github.com>
2018-10-01 19:41:32 +0900

    201724444김지원
    Chap3Assignment.
    two_func.cpp is assignment file.
    201724444 김지원

--- CProgChap3Assignment22/CProgChap3Assignment2/2.3 two_func.cpp
+++ CProgChap3Assignment22/CProgChap3Assignment2/2.3 two_func.cpp
@@ -1,17 +1,11 @@
-//* two_func.c -- a program using two functions in one file */
-// main()에서 butler() 함수 호출을 실행하여 확인
-#include <stdio.h>
-void butler(void);
-/* ANSI/ISO C function prototyping */
-int mmain(void) {
-	printf("I will summon the butler function.\n");
-	butler();
-	printf("Yes. Bring me some tea and writeable DVDs.\n");
-	getchar(); //butler() 호출로 변경하여 실습
+#include <stdio.h>  
+int main(void) {
+	float g, h;
+	float tax, rate;
+	rate = 0.08;
+	g = 1.0e5;
+	tax = rate * g;
+	h = g + tax;
+	printf("You owe $%f plus $%f in taxes for a total of $%f.\n", g, tax, h);
 	return 0;
-}
-
-void butler(void)       /* start of function definition */
-{
-	printf("You rang, sir?\n");
 }
